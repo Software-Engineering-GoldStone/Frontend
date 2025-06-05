ecec8a764aa8bd022bb9c4d354ca6956ac06df3d
박지호 <jiho5993@gmail.com>
2025-06-05 11:59:50 +0000

    feat: socket io 연동 및 테스트

--- src/main.ts
+++ src/main.ts
@@ -17,24 +17,25 @@ axios.defaults.baseURL = 'http://172.17.78.133:8080'
 
 // socket.io-client import 및 소켓 생성(백엔드와 실시간 연결)
 import { io, Socket } from 'socket.io-client'
-const socket: Socket = io('ws://172.17.78.133:3031', {transports: ['websocket'], reconnection: false})
+const socket: Socket = io('ws://localhost:3031', { transports: ['websocket'], reconnection: false })
 
 // 소켓 연결 완료 이벤트 핸들러
 socket.on('connect', () => {
-    console.log('Socket connected with id:', socket.id);
-});
+  console.log('Socket connected with id:', socket.id)
+})
 
 socket.on('connect_error', (err) => {
-    console.error('Socket connection error in main.ts:', err);
-});
+  console.error('Socket connection error in main.ts:', err)
+})
 
 // 아이콘 라이브러리에 추가
 library.add(faPenToSquare, faXmark)
 
 const app = createApp(App)
 
-// 전역 프로퍼티로 socket 등록
+// 전역 프로퍼티 설정
 app.config.globalProperties.$socket = socket
+app.config.globalProperties.$defaultGameRoomId = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
 
 app.component('font-awesome-icon', FontAwesomeIcon) // 전역 컴포넌트 등록
 
